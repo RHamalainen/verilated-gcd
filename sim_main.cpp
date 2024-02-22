29c92889ad108cad6a267e9e4f980f2a220dd7ff
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-02-22 15:31:32 +0200

    Update sim_main.cpp

--- sim_main.cpp
+++ sim_main.cpp
@@ -1,6 +1,19 @@
+#include <Vtop.h>
 #include <iostream>
+#include <memory>
+#include <verilated.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   std::cout << "hello from " << __FILE__ << std::endl;
+  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+  contextp->commandArgs(argc, argv);
+  const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+  while (!contextp->gotFinish())
+  {
+    contextp->timeInc(1);
+    top->eval();
+  }
+  top->final();
   return 0;
 }
