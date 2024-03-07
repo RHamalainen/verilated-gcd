97d3a51449aef971648101836e922a3e08391ae9
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-03-07 12:05:57 +0200

    Update top.v

--- top.v
+++ top.v
@@ -1,6 +1,16 @@
-module top;
+module top (
+        input clk,
+        input rst,
+        input [3:0] in1,
+        input [3:0] in2,
+        input ena,
+        output [3:0] out,
+        output rdy);
+    
+    gcd gcd(.clk, .rst, .in1, .in2, .ena, .out, .rdy);
+
     initial begin
-        $display("Hello, World!");
-        $finish;
+        $display("hello from ", `__FILE__);
+        //$finish;
     end
 endmodule
